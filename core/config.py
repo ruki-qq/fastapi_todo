eb28a082e6dd6fda3b3512e0492f7a6849b9a4bb
ruki-qq <ruki@duck.com>
2024-04-17 12:23:08 +0300

    Add config

---
+++ core/config.py
@@ -0,0 +1,32 @@
+from typing import Final
+
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class EnvVars(BaseModel):
+    db_user: str
+    db_pass: str
+    db_host: str
+    db_port: int
+    db_name: str
+
+
+class DBSettings(EnvVars, BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    @property
+    def url(self) -> str:
+        return f"postgresql+asyncpg://{self.db_user}:{self.db_pass}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+    echo: bool = True
+
+
+class Settings(DBSettings):
+    api_v1_prefix: str = "/api/v1"
+
+
+settings = Settings()
+
+if __name__ == "__main__":
+    print(settings.url)
