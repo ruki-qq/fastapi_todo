30183dbfe140c0611eb052ed86dbb13b60378718
ruki-qq <ruki@duck.com>
2024-04-17 16:11:02 +0300

    Rename notes tb migration

--- alembic/versions/2024_04_17_1610-a5860c1cfddb_add_notes_model.py
+++ alembic/versions/2024_04_17_1610-a5860c1cfddb_add_notes_model.py
@@ -1,8 +1,8 @@
-"""Add count column to association tab
+"""Add notes model
 
-Revision ID: 38d7a110a84c
+Revision ID: a5860c1cfddb
 Revises: 
-Create Date: 2024-04-17 16:02:19.556540
+Create Date: 2024-04-17 16:10:32.108258
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '38d7a110a84c'
+revision: str = 'a5860c1cfddb'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
