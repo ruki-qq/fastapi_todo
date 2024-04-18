e57865efc976bf8bb6f805d81fbb9ab8933a1471
ruki-qq <ruki@duck.com>
2024-04-18 16:36:30 +0300

    Add crud for notes and tasks

--- api_v1/notes/crud.py
+++ api_v1/notes/crud.py
@@ -1,27 +1,6 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from api_v1 import crud_helper
+from api_v1.utils import BaseObjectCRUD, BaseObjectDependencies
 from api_v1.models import Note
-from api_v1.notes.schemas import NoteCreate, NoteUpdate
 
 
-async def get_notes(session: AsyncSession) -> list[Note]:
-    return await crud_helper.get_objects(session, Note)
-
-
-async def get_note(session: AsyncSession, note_id: int) -> Note | None:
-    return await crud_helper.get_object(session, Note, note_id)
-
-
-async def create_note(session: AsyncSession, note_in: NoteCreate) -> Note:
-    return await crud_helper.create_object(session, Note, note_in)
-
-
-async def update_note(
-    session: AsyncSession, note: Note, note_update: NoteUpdate
-) -> Note:
-    return await crud_helper.update_object(session, note, note_update)
-
-
-async def delete_note(session: AsyncSession, note: Note) -> None:
-    await crud_helper.delete_object(session, note)
+note_crud = BaseObjectCRUD(Note)
+note_dependencies = BaseObjectDependencies(note_crud, "note")
--- api_v1/tasks/crud.py
+++ api_v1/tasks/crud.py
@@ -1,27 +1,6 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from api_v1 import crud_helper
+from api_v1.utils import BaseObjectCRUD, BaseObjectDependencies
 from api_v1.models import Task
-from api_v1.tasks.schemas import TaskCreate, TaskUpdate
 
 
-async def get_tasks(session: AsyncSession) -> list[Task]:
-    return await crud_helper.get_objects(session, Task)
-
-
-async def get_task(session: AsyncSession, task_id: int) -> Task | None:
-    return await crud_helper.get_object(session, Task, task_id)
-
-
-async def create_task(session: AsyncSession, task_in: TaskCreate) -> Task:
-    return await crud_helper.create_object(session, Task, task_in)
-
-
-async def update_task(
-    session: AsyncSession, task: Task, task_update: TaskUpdate
-) -> Task:
-    return await crud_helper.update_object(session, task, task_update)
-
-
-async def delete_task(session: AsyncSession, task: Task) -> None:
-    await crud_helper.delete_object(session, task)
+task_crud = BaseObjectCRUD(Task)
+task_dependencies = BaseObjectDependencies(task_crud, "task")
