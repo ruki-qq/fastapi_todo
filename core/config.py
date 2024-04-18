87669b5831b760527139bef96d83e1af89136ed4
ruki-qq <ruki@duck.com>
2024-04-18 16:35:20 +0300

    Revert config changes

--- core/config.py
+++ core/config.py
@@ -24,17 +24,10 @@ class DBSettings(EnvVars, BaseSettings):
 
 
 class Settings(DBSettings):
-    _instance = None
-
     api_v1_prefix: str = "/api/v1"
-
-    def __new__(cls):
-        if not cls._instance:
-            cls._instance = super().__new__(cls)
-        return cls._instance
 
 
 settings = Settings()
 
 if __name__ == "__main__":
-    print(settings.url)
+    print(settings.db_url)
